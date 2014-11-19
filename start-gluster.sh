41c1180671b5af7095253de8e73f383086252bdc
Florin Peter <github@florin-peter.de>
2014-11-19 21:21:30 +0100

    Update start-gluster.sh

--- start-gluster.sh
+++ start-gluster.sh
@@ -1,4 +1,4 @@
- Make sure rpcbind is started - GlusterNFS would complain
+## Make sure rpcbind is started - GlusterNFS would complain
 ## RPC bind service will complain about Locale files but ignore
 service rpcbind start
 
@@ -6,7 +6,7 @@ service rpcbind start
 IPADDR=$(hostname -i)
 
 ## Change this to your name if necessary
-VOLUME=myvolume
+VOLUME=vol
 
 ## Start Gluster Management Daemon
 service glusterd start
@@ -19,40 +19,7 @@ fi
 
 if [ ! -d "/var/lib/glusterd/vols/$VOLUME" ]; then
   ## Always create a sub-directory inside a mount-point
-  gluster --mode=script --wignore volume create $VOLUME $IPADDR:/mnt/vault/$VOLUME
-fi
-
-gluster --mode=script --wignore volume start $VOLUME force
-
-shutdown_gluster()
-{
-  service glusterd stop
-  exit $?
-}
-
-trap shutdown_gluster SIGINT
-while true; do sleep 1; done
-# Make sure rpcbind is started - GlusterNFS would complain
-## RPC bind service will complain about Locale files but ignore
-service rpcbind start
-
-## On docker surprisingly hostnames are mapped to IP's :-)
-IPADDR=$(hostname -i)
-
-## Change this to your name if necessary
-VOLUME=myvolume
-
-## Start Gluster Management Daemon
-service glusterd start
-if [ -z $VOLUME ]; then
-  ## Check if volume is null
-  service glusterd stop
-  exit 255
-fi
-
-if [ ! -d "/var/lib/glusterd/vols/$VOLUME" ]; then
-  ## Always create a sub-directory inside a mount-point
-  gluster --mode=script --wignore volume create $VOLUME $IPADDR:/mnt/vault/$VOLUME
+  gluster --mode=script --wignore volume create $VOLUME $IPADDR:/mnt/brick
 fi
 
 gluster --mode=script --wignore volume start $VOLUME force
